f1bd073617169bb5fbaaa6ae20597cfdb4b5fa5f
Lap Luu Chi <laplc@Laps-MacBook-Pro.local>
2025-12-26 10:06:46 +0700

    Fix 404 error when login

--- frontend/app-ui/next.config.ts
+++ frontend/app-ui/next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
       },
     ],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/api/:path*',
+        destination: 'http://localhost:3000/api/:path*',
+      },
+    ];
+  },
 };
 
 export default nextConfig;
